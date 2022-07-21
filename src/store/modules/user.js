f472724ae56eba851ddd04044c7eff8f27b8c776
陈惜缘 <2264119591@qq.com>
2022-07-21 17:46:57 +0800

    完成登录页面

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,40 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+
+// 状态
+const state = {
+  token: getToken()
+}
+// 修改状态
+const mutations = {
+  // 修改token的方法
+  setToken(state, token) {
+    state.token = token // 修改了vuex状态
+    setToken(token) // 这里的setToken是从utils里面的auth.js文件里面调用的
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  }
+}
+// 执行异步
+const actions = {
+  // 登录请求-->commit-->mutations
+  async login(context, data) {
+    // 调用登录的请求接口
+    const res = await login(data)
+    // 打印接口调用结果
+    // console.log(res.data.data)
+
+    // 获取到token存储到vuex/cookie
+    context.commit('setToken', res)
+  }
+}
+const getters = {}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions,
+  getters
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,22 @@
   <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
+      <!-- 放置标题图片 @是设置的别名-->
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">
+          <img src="@/assets/common/login-logo.png" alt="">
+        </h3>
       </div>
 
-      <el-form-item prop="username">
+      <el-form-item prop="mobile">
         <span class="svg-container">
           <svg-icon icon-class="user" />
         </span>
         <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
+          ref="mobile"
+          v-model="loginForm.mobile"
+          placeholder="请输入手机号"
+          name="mobile"
           type="text"
           tabindex="1"
           auto-complete="on"
@@ -30,7 +33,7 @@
           ref="password"
           v-model="loginForm.password"
           :type="passwordType"
-          placeholder="Password"
+          placeholder="请输入密码"
           name="password"
           tabindex="2"
           auto-complete="on"
@@ -41,11 +44,11 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button class="loginBtn" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
+        <span style="margin-right:20px;">账号: 13800000002</span>
+        <span> 密码: 123456</span>
       </div>
 
     </el-form>
@@ -53,33 +56,48 @@
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
-
+// import { validUsername } from '@/utils/validate'
+import { mapActions } from 'vuex' // 引入vuex的辅助函数
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
-      } else {
-        callback()
-      }
-    }
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
-      } else {
-        callback()
-      }
-    }
+    // const validateUsername = (rule, value, callback) => {
+    //   if (!validUsername(value)) {
+    //     callback(new Error('Please enter the correct user name'))
+    //   } else {
+    //     callback()
+    //   }
+    // }
+    // const validatePassword = (rule, value, callback) => {
+    //   if (value.length < 6) {
+    //     callback(new Error('The password can not be less than 6 digits'))
+    //   } else {
+    //     callback()
+    //   }
+    // }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        mobile: '13800000002',
+        password: '123456'
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        mobile: [
+          { required: true, trigger: 'blur', message: '手机号不能为空' },
+          {
+            pattern: /^1[3-9]\d{9}$/,
+            message: '手机号格式不正确',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, trigger: 'blur', message: '密码不能为空' },
+          {
+            min: 6,
+            max: 16,
+            message: '密码6-16位之间',
+            trigger: 'blur'
+          }
+        ]
       },
       loading: false,
       passwordType: 'password',
@@ -95,6 +113,7 @@ export default {
     }
   },
   methods: {
+    ...mapActions('user', ['login']),
     showPwd() {
       if (this.passwordType === 'password') {
         this.passwordType = ''
@@ -105,21 +124,37 @@ export default {
         this.$refs.password.focus()
       })
     },
-    handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
+    async handleLogin() {
+      // console.log(11)
+      try {
+        await this.$refs.loginForm.validate()
+        this.loading = true
+
+        // 登录请求-->action
+        // action也是一个promise
+        await this.login(this.loginForm)
+
+        // 成功了
+        // 跳转主页、关闭loading
+        this.$router.push({ path: this.redirect || '/' })
+        this.loading = false
+      } catch (e) {
+        this.loading = false
+      }
+      // this.$refs.loginForm.validate(valid => {
+      //   if (valid) {
+      //     this.loading = true
+      //     this.$store.dispatch('user/login', this.loginForm).then(() => {
+      //       this.$router.push({ path: this.redirect || '/' })
+      //       this.loading = false
+      //     }).catch(() => {
+      //       this.loading = false
+      //     })
+      //   } else {
+      //     console.log('error submit!!')
+      //     return false
+      //   }
+      // })
     }
   }
 }
@@ -130,7 +165,7 @@ export default {
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
 $bg:#283443;
-$light_gray:#fff;
+$light_gray: #68b0fe;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -164,10 +199,17 @@ $cursor: #fff;
   }
 
   .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    // border: 1px solid rgba(255, 255, 255, 0.1);
+    // background: rgba(0, 0, 0, 0.1);
+    // border-radius: 5px;
+    // color: #454545;
+     border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(255, 255, 255, 0.7); // 输入登录表单的背景色
     border-radius: 5px;
     color: #454545;
+    .el-form-item__error {
+    color: #fff
+  }
   }
 }
 </style>
@@ -175,19 +217,26 @@ $cursor: #fff;
 <style lang="scss" scoped>
 $bg:#2d3a4b;
 $dark_gray:#889aa4;
-$light_gray:#eee;
+$light_gray: #68b0fe;
 
 .login-container {
   min-height: 100%;
   width: 100%;
   // background-color: $bg;
-  background: url(./6b67d34f77f6dc420937bd01d922d7a.jpg) no-repeat;
-  background-size: 1280px;
+  background-image: url('~@/assets/common/6b67d34f77f6dc420937bd01d922d7a.jpg');
+  background-size: 1900px;
+   background-position: center; // 将图片位置设置为充满整个屏幕
   overflow: hidden;
+  .loginBtn {
+  background: #407ffe;
+  height: 60px;
+  line-height: 32px;
+  font-size: 24px;
+}
 
   .login-form {
     position: relative;
-    width: 520px;
+    width: 480px;
     max-width: 100%;
     padding: 160px 35px 0;
     margin: 0 auto;
@@ -218,9 +267,9 @@ $light_gray:#eee;
     position: relative;
 
     .title {
-      font-size: 26px;
+      font-size: 20px;
       color: $light_gray;
-      margin: 0px auto 40px auto;
+      margin: 0px auto 25px auto;
       text-align: center;
       font-weight: bold;
     }
